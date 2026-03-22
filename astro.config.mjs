8284fe2be5e54331f89a2e67efdb7d4b365737d5
Jonathan Berger <drskullster@pm.me>
2026-03-22 15:28:43 +0100

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://overflow.gallery',
     i18n: {
         locales: ['en', 'fr'],
         defaultLocale: 'fr',
